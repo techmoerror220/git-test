d83d101cb9abfb80a7163d38395840433ea07863
dgm@laptop <daniel.guinea.uned@gmail.com>
2019-12-31 11:14:51 +0100

    My fourth commit

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 inta main()
 {
-        printf("%s\n", "New phrase to make my third commit");
+        printf("%s\n", "This is incredible. But I'll change it to this is my fourth edit!!");
         return 0;
 }
